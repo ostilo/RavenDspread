83dd901674df83afa607c388774c93fbdc005093
ayodejiolalekan <olalekanayodeji79@gmail.com>
2023-04-04 13:48:13 +0100

    SDK Added

---
+++ app/src/main/java/com/ravenpos/ravendspread/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ravenpos.ravendspread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
