f73604db5b3bcbc700142cba384962cc1e9f73dc
ayodejiolalekan <olalekanayodeji79@gmail.com>
2023-04-10 21:11:07 +0100

    Develop

--- ravendspreadpos/src/main/java/com/ravenpos/ravendspreadpos/device/MainActivity.java
+++ ravendspreadpos/src/main/java/com/ravenpos/ravendspreadpos/device/MainActivity.java
@@ -1968,7 +1968,7 @@ public class MainActivity extends BaseActivity implements ShowGuideView.onGuideV
                 // here to request the missing permissions, and then overriding
                 //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
                 //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
+                // to handle the case whehgoore the user grants the permission. See the documentation
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }
